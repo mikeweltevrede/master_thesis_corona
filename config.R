a2a9008f4b0d2ec2e1262491b6c6eb2b22ed71ea
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2020-04-15 10:38:16 +0200

    Activate environment in config

--- config.R
+++ config.R
@@ -1,5 +1,18 @@
-data_path = "data"
+# Environment variables
 env_name = "r-thesis_corona"
+path_requirements = "requirements.txt"
+
+# Activate the Conda environment. If it does not exist yet, create it with the
+# required packages.
+tryCatch(use_condaenv(env_name),
+         error=function(e){
+           requirements = scan(file=path_requirements, what=character(),
+                               quiet=TRUE) 
+           conda_create(env_name, packages=requirements)
+         })
+
+# Path variables for data
+data_path = "data"
 path_wiki = paste0(data_path, "/italy_wikipedia.xlsx")
 path_cleaned_wide = paste0(data_path, "/italy_wikipedia_cleaned.csv")
 path_full_wide = paste0(data_path, "/italy_wikipedia_full_wide.csv")
