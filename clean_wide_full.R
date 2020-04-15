d1414d7b0b506911390df2cbd61da45db6ad776a
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2020-04-15 10:47:53 +0200

    Incorporate config

--- clean_wide_full.R
+++ clean_wide_full.R
@@ -5,18 +5,8 @@ library(readxl, quietly=TRUE)
 library(tidyverse, quietly=TRUE)
 library(reticulate, quietly=TRUE)
 
-data_path = "data"
-
-# Activate the Conda environment. If it does not exist yet, create it with the
-# required packages.
-env_name = "r-thesis_corona"
-
-tryCatch(use_condaenv(env_name),
-         error=function(e){
-           requirements = scan(file="requirements.txt", what=character(),
-                               quiet=TRUE) 
-           conda_create(env_name, packages=requirements)
-         })
+# Import standard variables
+source("config.R")
 
 # We need to clean the Wikipedia data before being able to process it in R, also
 # to include new dates. Run the next line to do so (you may need to
@@ -24,9 +14,8 @@ tryCatch(use_condaenv(env_name),
 py_run_file("clean_wide.py")
 
 # Read in the cleaned Wikipedia data
-df_wide = read_csv(paste0(data_path, "/italy_wikipedia_cleaned.csv"),
-                   col_types = do.call(
-                     cols, list(Date=col_date(format="%Y-%m-%d"))))
+df_wide = read_csv(path_cleaned_wide, col_types = do.call(
+  cols, list(Date=col_date(format="%Y-%m-%d"))))
 
 # We now add missing dates to the data for equal spacing.
 all_dates = seq.Date(min(df_wide$Date), max(df_wide$Date), by="day")
@@ -46,15 +35,13 @@ named_cols_replace_0 = set_names(as.list(rep(0, length(cols_replace_0))),
 cols_fill = colnames(df_wide)[
   which(!colnames(df_wide) %in% cols_replace_0)][-1]
 
+# Complete the dates and execute the filling procedure described above
 df_wide = df_wide %>%
   complete(Date = all_dates) %>%
   replace_na(named_cols_replace_0) %>%
   fill(all_of(cols_fill))
 
-# Save the tibble to a file
-write_csv(df_wide, paste0(data_path, "/italy_wikipedia_full_wide.csv"))
-
-# Convert the data to wide and join it with the extra data (containing the
+# Convert the data to long and join it with the extra data (containing the
 # amount of active ICU patients, recoveries, tested people, and positively
 # tested people)
 df_long = df_wide %>%
@@ -63,14 +50,17 @@ df_long = df_wide %>%
                names_to = c("group", ".value"),
                names_sep = "_")
 
-df_extra_long = read_excel(
-  paste0(data_path, "/italy_wikipedia.xlsx"), sheet="Extra") %>%
+df_extra = read_excel(path_wiki, sheet="Extra") %>%
   mutate(Date = as.Date(Date, format = "%Y-%m-%d")) %>%
-  drop_na() %>%
-  pivot_longer(cols = -Date,
-               names_to = c("group", ".value"),
-               names_sep = "_")
+  drop_na()
 
+df_extra_long = df_extra %>% pivot_longer(cols = -Date,
+                                          names_to = c("group", ".value"),
+                                          names_sep = "_")
+
+df_wide_full = full_join(df_wide, df_extra, by="Date")
 df_long_full = full_join(df_long, df_extra_long, by=c("Date", "group"))
 
-write_csv(df_long_full, paste0(data_path, "/italy_wikipedia_full_long.csv"))
+# Save the tibbles to a file
+write_csv(df_wide_full, path_full_wide)
+write_csv(df_long_full, path_full_long)
