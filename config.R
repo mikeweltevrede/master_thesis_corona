ca6e585f84c8acde913dc7c990aeb5634d1cbfc5
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2020-04-15 10:25:43 +0200

    Create configuration file

---
+++ config.R
@@ -0,0 +1,7 @@
+data_path = "data"
+env_name = "r-thesis_corona"
+path_wiki = paste0(data_path, "/italy_wikipedia.xlsx")
+path_cleaned_wide = paste0(data_path, "/italy_wikipedia_cleaned.csv")
+path_full_wide = paste0(data_path, "/italy_wikipedia_full_wide.csv")
+path_full_long = paste0(data_path, "/italy_wikipedia_full_long.csv")
+path_full_eurostat = paste0(data_path, "/merged_eurostat.csv")
