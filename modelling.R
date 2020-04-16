0ea7809b51336677747ae6bd06d9c8d79969330b
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2020-04-16 12:29:03 +0200

    Fix df_distance save and add Eurostat columns

--- modelling.R
+++ modelling.R
@@ -17,17 +17,22 @@ df_long = read_csv(path_full_long, col_types = do.call(
   cols, list(Date = col_date(format = "%Y-%m-%d"))))
 
 # Weighting matrix: distance between the largest cities
-df_distance = read_excel(path_wiki, sheet = "Distances") %>%
-  left_join(df_meta[, c("Code", "LargestCity")],
-            by=c("Distance (km)"="LargestCity")) %>%
-  column_to_rownames("Code") %>%
-  select(-"Distance (km)") %>%
-  data.matrix()
-colnames(df_distance) = rownames(df_distance)
+if (file.exists(path_distances)) {
+  load(path_distances)
+} else {
+  df_distance = read_excel(path_wiki, sheet = "Distances") %>%
+    left_join(df_meta[, c("Code", "LargestCity")],
+              by=c("Distance (km)"="LargestCity")) %>%
+    column_to_rownames("Code") %>%
+    select(-"Distance (km)") %>%
+    data.matrix()
+  colnames(df_distance) = rownames(df_distance)
+  save(df_distance, file = path_distances)
+}
 
 # Regressors: eurostat data, which are assumed to be time-constant
 # To merge all Eurostat zip files, uncomment the next line if the file does not
-# yet exist.
+# yet exist or if new data gets added.
 # py_run_file("eurostat_reader.py")
 
 df_eurostat = read_csv(path_full_eurostat, col_types = do.call(
@@ -37,6 +42,17 @@ df_eurostat = read_csv(path_full_eurostat, col_types = do.call(
 # entire country.
 df_eurostat = df_eurostat[sapply(df_eurostat$region,
                                  function(x){x %in% df_meta$Region}), ]
+
+eurostat_variables = c("air_passengers_arrived", "air_passengers_departed",
+                       "tourist_arrivals", "broadband_access", "available_beds",
+                       "maritime_passengers_disembarked",
+                       "maritime_passengers_embarked",
+                       "risk_of_poverty_or_social_exclusion")
+X = df_eurostat[, eurostat_variables]
+
+X %>%
+  mutate(tourist_arrivals = tourist_arrivals/sum(tourist_arrivals))
+
 
 # TODO: Process the Eurostat data so that we can use these as regressors.
 df_rail_travel = read_csv(path_interpolated_rail, col_types = do.call(
