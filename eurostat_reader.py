bce05e29eb6578416632d29db351f4cfb02676c7
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2020-04-20 21:21:19 +0200

    Remove underscores from column names and PEP8

--- eurostat_reader.py
+++ eurostat_reader.py
@@ -1,4 +1,6 @@
-import zipfile, glob, os
+import zipfile
+import glob
+import os
 from functools import reduce
 
 import pandas as pd
@@ -6,9 +8,10 @@ import numpy as np
 
 keep_time = False
 
-def eurostat_reader(file_path, na_proportion = 0.8, 
+
+def eurostat_reader(file_path, na_proportion=0.8,
                     cols_to_drop={"all": {'UNIT'},
-                                  "arrivals_at_tourist_accommodation_establishments.zip" : {'C_RESID'},
+                                  "arrivals_at_tourist_accommodation_establishments.zip": {'C_RESID'},
                                   "average_length_of_stay_at_hospitals.zip": {'SEX', 'AGE', 'ICD10'},
                                   "crude_death_rate.zip": {'SEX', 'AGE'},
                                   "disposable_income_per_inhabitant.zip": {'DIRECT'},
@@ -20,33 +23,35 @@ def eurostat_reader(file_path, na_proportion = 0.8,
             if 'Data' in file:
                 with zip_file.open(file) as data_file:
                     df = pd.read_csv(data_file, encoding="ISO-8859-1")
-    
-    base_file_path = os.path.basename(file_path)        
+
+    base_file_path = os.path.basename(file_path)
     # Drop desired columns
     if base_file_path in cols_to_drop.keys():
         if (len(cols_to_drop[base_file_path].intersection(df.columns)) > 0):
             df = df.drop(columns=cols_to_drop[base_file_path].intersection(df.columns))
     df = df.drop(columns=cols_to_drop["all"])
-        
+
     # Clean Value column and convert to float
     df["Value"] = df["Value"].apply(
         lambda x: x.replace(",", "")).replace({":": None}, regex=False).astype(np.float32)
-    
+
     # Check if there is no data for certain years and then drop those years
     na_check = df.set_index('TIME')['Value'].isna().all(level=0)
     if len(na_check.index[na_check]) > 0:
         df = df.set_index('TIME').drop(list(na_check.index[na_check])).reset_index()
-    
+
     # Find the column containing the relevant values to spread on
     value_col = [col for col in df.columns if col not in {"TIME", "GEO", "Value"}]
-    
+
     if len(value_col) > 1:
         raise ValueError(f"Too many columns available to spread on for file '{file_path}', "
-                         f"namely {value_col}. Check the data and add columns to remove to cols_to_drop.")  
-    
+                         f"namely {value_col}. Check the data and add columns to remove to "
+                         "cols_to_drop.")
+
     # Pivot from long to wide
     if len(value_col) == 1:
-        df = df.pivot_table(index=['TIME','GEO'], columns=value_col[0], values='Value').reset_index()
+        df = df.pivot_table(index=['TIME', 'GEO'], columns=value_col[0],
+                            values='Value').reset_index()
         try:
             del df.columns.name
         except AttributeError:
@@ -55,22 +60,23 @@ def eurostat_reader(file_path, na_proportion = 0.8,
     test = (df.groupby('TIME').apply(lambda x: len(x) - x.isna().sum()) > 0).all(axis=1)
     max_year = test[test].index.max()
     df = df.set_index("TIME").loc[max_year].reset_index()
-    
+
     # Drop fully NA rows and columns
     df = df.dropna(how='all').dropna(axis=1, how='all')
     na_props = df.isna().sum().divide(df.apply(len))
-    
+
     if len(na_props[na_props > na_proportion].index) > 0:
         print(f"Over {na_proportion*100} percent NAs in the column(s) "
               f"{na_props[na_props > na_proportion].index} in file {base_file_path}. "
               "Dropping these columns")
-    
+
         df = df.drop(columns=na_props[na_props > na_proportion].index)
-    
+
     if len(value_col) == 0:
         df = df.rename(columns={'Value': base_file_path.replace('.zip', '')})
-    
+
     return df
+
 
 dfs = []
 
@@ -80,9 +86,9 @@ for file in glob.glob("data/eurostat/*.zip"):
     if 'TOCLEAN' in file:
         # TODO: Skip these files for now. These are per NUTS3 region and need to be aggregated.
         continue
-    
+
     df = eurostat_reader(file)
-    
+
     if keep_time:
         dfs.append(df)
     else:
@@ -97,7 +103,8 @@ else:
                        dfs).sort_values(by=['GEO'])
 
 # Drop extra regions
-extra_region = (df_merged["GEO"] == "Extra-Regio NUTS 1") | (df_merged["GEO"] == "Extra-Regio NUTS 2")
+extra_region = (df_merged["GEO"] ==
+                "Extra-Regio NUTS 1") | (df_merged["GEO"] == "Extra-Regio NUTS 2")
 df_merged = df_merged.drop(extra_region[extra_region].index)
 
 # Propagate constant values over years for each region - Total area
@@ -118,50 +125,55 @@ if keep_time:
 
 dict_rename = {
     'GEO': 'region',
-    'Freight and mail loaded': 'air_freight_loaded',
-    'Freight and mail unloaded': 'air_freight_unloaded',
-    'Passengers carried (arrival)': 'air_passengers_arrived',
-    'Passengers carried (departures)': 'air_passengers_departed',
+    'Freight and mail loaded': 'airFreightLoaded',
+    'Freight and mail unloaded': 'airFreightUnloaded',
+    'Passengers carried (arrival)': 'airPassengersArrived',
+    'Passengers carried (departures)': 'airPassengersDeparted',
     'Total area': 'area',
-    'Hotels; holiday and other short-stay accommodation; camping grounds, recreational vehicle parks and trailer parks': 'tourist_arrivals',
-    'In-patient average length of stay (in days)': 'hospital_stay',
-    'Malignant neoplasms (C00-C97)': 'death_rate_cancer',
-    'Diabetes mellitus': 'death_rate_diabetes',
-    'Ischaemic heart diseases': 'death_rate_chd',
-    'Influenza (including swine flu)': 'death_rate_influenza',
-    'Pneumonia': 'death_rate_pneumonia',
-    'Disposable income, net': 'disposable_income',
-    'Medical doctors': 'medical_doctors',
-    'Nurses and midwives': 'nurses_midwives',
-    'Available beds in hospitals (HP.1)': 'available_beds',
-    'Curative care beds in hospitals (HP.1)': 'curative_care_beds',
-    'Long-term care beds in hospitals (HP.1)': 'longterm_care_beds',
-    'Other beds in hospitals (HP.1)': 'other_beds',
-    'Psychiatric care beds in hospitals (HP.1)': 'psychiatric_care_beds',
-    'Rehabilitative care beds in hospitals (HP.1)': 'rehabilitative_care_beds',
-    'Internet use: interaction with public authorities (last 12 months)': 'internet_contact_authorities',   
-    'Electrified railway lines': 'length_electrified_railway',
-    'Motorways': 'length_motorways',
-    'Navigable canals': 'length_canals',
-    'Navigable rivers': 'length_rivers',
-    'Other roads': 'length_other_roads',
-    'Railway lines with double and more tracks': 'length_large_railway',
-    'Total railway lines': 'length_railway',
-    'Freight loaded': 'maritime_freight_loaded',
-    'Freight unloaded': 'maritime_freight_unloaded',
-    'Passengers disembarked': 'maritime_passengers_disembarked',
-    'Passengers embarked': 'maritime_passengers_embarked',
-    'Median age of population': 'median_age',
-    'Less than primary, primary and lower secondary education (levels 0-2)': 'lower_education',
-    'Upper secondary and post-secondary non-tertiary education (levels 3 and 4)': 'higher_education',
-    'Tertiary education (levels 5-8)': 'tertiary_education'
+    ('Hotels; holiday and other short-stay accommodation; camping grounds, recreational vehicle '
+     'parks and trailer parks'): 'touristArrivals',
+    'In-patient average length of stay (in days)': 'hospitalStay',
+    'broadband_access': 'broadbandAccess',
+    'Malignant neoplasms (C00-C97)': 'deathRateCancer',
+    'Diabetes mellitus': 'deathRateDiabetes',
+    'Ischaemic heart diseases': 'deathRateChd',
+    'Influenza (including swine flu)': 'deathRateInfluenza',
+    'Pneumonia': 'deathRatePneumonia',
+    'Disposable income, net': 'disposableIncome',
+    'gdp_per_inhabitant': 'gdpPerInhabitant',
+    'Medical doctors': 'medicalDoctors',
+    'Nurses and midwives': 'nursesMidwives',
+    'Available beds in hospitals (HP.1)': 'availableBeds',
+    'Curative care beds in hospitals (HP.1)': 'curativeCareBeds',
+    'Long-term care beds in hospitals (HP.1)': 'longtermCareBeds',
+    'Other beds in hospitals (HP.1)': 'otherBeds',
+    'Psychiatric care beds in hospitals (HP.1)': 'psychiatricCareBeds',
+    'Rehabilitative care beds in hospitals (HP.1)': 'rehabilitativeCareBeds',
+    'Internet use: interaction with public authorities (last 12 months)': 'internetContactAuthorities',
+    'Electrified railway lines': 'lengthElectrifiedRailway',
+    'Motorways': 'lengthMotorways',
+    'Navigable canals': 'lengthCanals',
+    'Navigable rivers': 'lengthRivers',
+    'Other roads': 'lengthOtherRoads',
+    'Railway lines with double and more tracks': 'lengthLargeRailway',
+    'Total railway lines': 'lengthRailway',
+    'Freight loaded': 'maritimeFreightLoaded',
+    'Freight unloaded': 'maritimeFreightUnloaded',
+    'Passengers disembarked': 'maritimePassengersDisembarked',
+    'Passengers embarked': 'maritimePassengersEmbarked',
+    'Median age of population': 'medianAge',
+    'Less than primary, primary and lower secondary education (levels 0-2)': 'lowerEducation',
+    'Upper secondary and post-secondary non-tertiary education (levels 3 and 4)': 'higherEducation',
+    'Tertiary education (levels 5-8)': 'tertiaryEducation',
+    'population_density': 'populationDensity',
+    'population_numbers': 'populationNumbers',
+    'risk_of_poverty_or_social_exclusion': 'riskOfPovertyOrSocialExclusion'
 }
 
 if keep_time:
     dict_rename['TIME'] = 'time'
 
 df_merged = df_merged.rename(columns=dict_rename)
-
 df_merged.to_csv("data/merged_eurostat.csv", index=False)
 
 # Inter-city railroad connections
