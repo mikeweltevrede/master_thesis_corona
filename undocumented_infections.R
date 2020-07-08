9ddb0e4411059b3ed50b35b678a06b0cfe74d77c
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2020-07-08 14:45:03 +0200

    Add data June 5

--- undocumented_infections.R
+++ undocumented_infections.R
@@ -4,7 +4,8 @@ library(glue)
 undocumented_infections = function(N, TC, form=c("linear", "quadratic",
                                                  "downwards_vertex",
                                                  "upwards_vertex", "cubic"),
-                                   gamma=2/5, gamma2=1/2){
+                                   beta=0.5, beta1=0.25, gamma=2/5, gamma2=1/2,
+                                   fmin=0){
   
   assert_that(!is.na(N) & !is.na(TC), msg=glue("N and TC cannot be NA."))
   
@@ -16,13 +17,21 @@ undocumented_infections = function(N, TC, form=c("linear", "quadratic",
     assert_that(!is.na(gamma),
                 msg=glue("For the functional form {form}, gamma cannot be NA."))
   
+    # TODO: These conditions are likely dependent on beta and fmin; possibly
+    # derive conditions and change this accordingly
     assert_that(gamma > 1/4 & gamma < 3/4,
                 msg=glue("For the functional form {form}, gamma should be ",
                          "above 1/4 and below 3/4."))
     
-    a = (2-4*gamma)/N^2
-    b = (4*gamma-1)/N
-    return(a*TC^2+b*TC)
+    # a = (2-4*gamma)/N^2
+    # b = (4*gamma-1)/N
+    # c = 0
+    
+    a = (beta - gamma + (1-beta)*fmin)/(beta*(1-beta)*N^2)
+    b = (gamma - beta^2 - (1-beta^2)*fmin)/(beta*(1-beta)*N)
+    c = fmin
+    
+    return(a*TC^2+b*TC+c)
     
   } else if (form == "downwards_vertex") {
     a = -1/N^2
