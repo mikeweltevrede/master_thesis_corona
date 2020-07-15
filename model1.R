1b6718031436ca858af5ed20b8908e748bf9f3fa
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2020-07-15 09:37:26 +0200

    Make alpha plots for AIC

--- model1.R
+++ model1.R
@@ -194,19 +194,20 @@ results_table = rbind(
 table_no_ms = xtable(results_table, math.style.exponents = TRUE)
 table_no_ms
 
-#### Models with model selection (BIC) ####
-results_table_ms = tibble(variables = c(all_variables, "alpha"))
+#### Models with model selection (AIC) ####
+results_table_ms_aic = tibble(variables = c(all_variables, "alpha"))
 
 #### National model ####
 # Construct formula
 # TODO: Idem dito
 fm = glue("infectivesTotal ~ lag(infectivesTotal, {lag}):",
           "lag(susceptibleRateTotal, {lag})+",
-           paste(X_regressors, collapse="+")) %>%
+          paste(X_regressors, collapse="+")) %>%
   as.formula
 
-# Use BIC for model selection - scope says we want to always keep
+# Use AIC for model selection - scope says we want to always keep
 # alpha_within in
+model = step(lm(fm, data=df_wide), k=2, trace=0,
              scope=list("lower" = glue("infectivesTotal ~ ",
                                        "lag(infectivesTotal, {lag}):",
                                        "lag(susceptibleRateTotal, {lag})") %>%
@@ -248,8 +249,8 @@ for (region in regions){
   # Select only the data for the relevant region
   data = df_long %>% filter(code == !!region)
   
-  # Use BIC for model selection
   # Use AIC for model selection
+  model = step(lm(fm, data=data), k=2, trace=0,
                scope=list("lower" = glue("{infective_variable} ~ ",
                                          "lag({infective_variable}, {lag}):",
                                          "lag(susceptibleRate, {lag})") %>%
@@ -459,7 +460,62 @@ for (sub_tbl in split(tbl_alpha, tbl_alpha$direction)){
          path=output_path, width = 10.8, height = 6.62, units = "in")
 }
 
-#### With model selection BIC ####
+#### With model selection (AIC) ####
+tbl_alpha = tibble(date = as.Date(NA), alphas=numeric(0), code=character(0))
+
+# Find the estimates of alpha per region over time
+for (region in regions){
+  alphas = vector("double")
+  dates = vector("character")
+  
+  # Select only the data for the relevant region
+  data = df_long %>% filter(code == !!region)
+  
+  for (t in start:nrow(data)){
+    # Use AIC for model selection - scope says we want to always keep
+    # alpha_within in
+    model = step(lm(fm, data=data[1:t, ]), k=2, trace=0,
+                 scope=list("lower" = glue("{infective_variable} ~ ",
+                                           "lag({infective_variable}, {lag}):",
+                                           "lag(susceptibleRate, {lag})") %>%
+                              as.formula,
+                            "upper" = fm))
+    model
+    
+    # Retrieve the alpha estimate and append this to the list of alphas
+    alpha = model$coefficients[[glue("lag({infective_variable}, {lag}):",
+                                     "lag(susceptibleRate, {lag})")]]
+    alphas = c(alphas, alpha)
+  }
+  
+  # Append the results to the table
+  tbl_alpha = tbl_alpha %>%
+    bind_rows(tibble(date = data$date[start:nrow(data)],
+                     alphas = alphas,
+                     code = region) %>%
+                drop_na())
+}
+
+# Add region and direction to the table
+tbl_alpha = tbl_alpha %>%
+  left_join(df_meta %>% select(c(region, code, direction)), by="code")
+
+# Make a plot per direction
+for (sub_tbl in split(tbl_alpha, tbl_alpha$direction)){
+  direc = sub_tbl$direction[1]
+  g = ggplot(sub_tbl, aes(date, alphas, colour = region)) + 
+    geom_point() +
+    geom_smooth(method="loess", span=0.3, se=FALSE) +
+    xlab("") +
+    ylab(TeX("$\\alpha_{within}$")) +
+    scale_colour_manual(values=c("#E69F00", "#56B4E9", "#009E73", "#0072B2",
+                                 "#D55E00", "#CC79A7"))
+  print(g)
+  ggsave(glue("model1_lag{lag}_alphawithin_{direc}_aic{undoc_flag}.pdf"),
+         path=output_path, width = 10.8, height = 6.62, units = "in")
+}
+
+#### With model selection (BIC) ####
 tbl_alpha = tibble(date = as.Date(NA), alphas=numeric(0), code=character(0))
 
 # Find the estimates of alpha per region over time
