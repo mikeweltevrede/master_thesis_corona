92c8aa257fdddd6ab176a8543b42c6f5b9f70d10
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2020-07-31 16:59:22 +0200

    Clean up

--- explore.R
+++ explore.R
@@ -10,8 +10,9 @@ library(GGally)
 source("config.R")
 
 # Read in metadata
-df_meta = readxl::read_xlsx(path_wiki, sheet = "Metadata")
+df_meta = readxl::read_xlsx(path_metadata, sheet = "Metadata")
 
+#### Make correlation matrix of discharge rates  ####
 # To merge all Eurostat zip files, uncomment the next line if the file does not
 # yet exist or if new data gets added.
 # reticulate::py_run_file("eurostat_reader.py")
@@ -26,9 +27,9 @@ dischargeRates = readr::read_csv(path_full_eurostat, col_types = do.call(
 
 GGally::ggcorr(dischargeRates, label=TRUE, hjust = 0.75, size = 4.5,
                layout.exp = 1, label_round = 2)
-ggsave("correlations_discharge_rates.png", path=output_path, dpi=300)
+ggsave("correlations_discharge_rates.pdf", path=output_path)
 
-
+#### Predict values ####
 # # Predict values -> Odd results with negative values and a completely different
 # # curve
 # fit_my_model = function(t, data) {
