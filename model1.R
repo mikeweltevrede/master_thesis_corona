394b7639218a35e2c829dc2759e6608df095eb96
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2020-08-13 14:37:35 +0200

    Clean up; update notation

--- model1.R
+++ model1.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 
 #### Model 1 - Within-region spread ####
-# I_rt = alpha_within*I_rt-tau*S_rt-tau + X_rt*delta + nu_rt
+# Delta X_rt = beta_within*Delta X_rt-tau*S_rt-tau + delta*M_rt + nu_rt
 
 #### Setup ####
 # Import standard variables
@@ -17,28 +17,8 @@ library(xtable)
 # Import data
 df_long = readr::read_csv(path_full_long, col_types = do.call(
   cols, list(date = col_date(format = "%Y-%m-%d"))))
-
-pivot_to_df_wide = function(df_long) {
-  # Turn long data into wide data
-  df_wide = df_long %>%
-    pivot_wider(names_from = code,
-                values_from = all_of(colnames(df_long)[-c(1,2)]))
-  
-  # Column names are now of the form "variable_regionCode". We want to have them
-  # in the form "regionCode_variable".
-  colnames(df_wide) = colnames(df_wide) %>%
-    sapply(function(s) {
-      s %>%
-        str_split("_", simplify = TRUE) %>%
-        rev %>%
-        paste(collapse="_")
-    }, USE.NAMES=FALSE)
-  
-  # Order the columns alphabetically, keeping date at the start
-  df_wide = df_wide[, c("date", sort(colnames(df_wide[-1])))]
-  
-  return(df_wide)
-}
+df_wide = readr::read_csv(path_full_wide, col_types = do.call(
+  cols, list(date = col_date(format = "%Y-%m-%d"))))
 
 # Latent period
 lag = 3
@@ -95,7 +75,7 @@ df_long = df_long %>%
 regions = unique(df_long$code)
 
 #### Models without model selection ####
-results_table = tibble(variables = c(all_variables, "alpha"))
+results_table = tibble(variables = c(all_variables, "beta"))
 
 #### National model ####
 # Construct formula
@@ -115,7 +95,7 @@ pvals = coef(summary(model))[, "Pr(>|t|)"] # TODO: SE with stars => Stargazer pa
 
 # Insert parameter estimates in the results table
 results_table = results_table %>%
-  left_join(tibble("variables" = c(all_variables, "alpha"),
+  left_join(tibble("variables" = c(all_variables, "beta"),
                    "National" = unname(
                      c(estimates[all_variables], estimates[
                        glue("lag(infectivesRateNational, {lag}):",
@@ -153,7 +133,7 @@ for (region in regions){
   
   # Insert parameter estimates in the results table
   results_table = results_table %>%
-    left_join(tibble("variables" = c(all_variables, "alpha"),
+    left_join(tibble("variables" = c(all_variables, "beta"),
                      !!glue("{region}") := unname(
                        c(estimates[all_variables], estimates[
                          glue("lag({infective_variable}, {lag}):",
@@ -169,7 +149,7 @@ for (region in regions){
 results_table = results_table %>%
   gather(region, val, 2:ncol(results_table)) %>%
   spread(names(results_table)[1], val) %>%
-  select(region, alpha, everything())
+  select(region, beta, everything())
 
 # Put the national results at the top
 results_table = rbind(
@@ -182,7 +162,7 @@ table_no_ms = xtable(results_table, math.style.exponents = TRUE)
 table_no_ms
 
 #### Models with model selection (AIC) ####
-results_table_aic = tibble(variables = c(all_variables, "alpha"))
+results_table_aic = tibble(variables = c(all_variables, "beta"))
 
 #### National model ####
 # Construct formula
@@ -192,7 +172,7 @@ fm = glue("infectivesNational ~ ",
   as.formula
 
 # Use AIC for model selection - scope says we want to always keep
-# alpha_within in
+# beta_within in
 model = step(lm(fm, data=df_wide), k=2, trace=0,
              scope=list("lower" = glue("infectivesNational ~ ",
                                        "lag(infectivesNational, {lag}):",
@@ -212,7 +192,7 @@ pvals = coef(summary(model))[, "Pr(>|t|)"] # TODO: SE with stars
 
 # Insert parameter estimates in the results table
 results_table_aic = results_table_aic %>%
-  left_join(tibble("variables" = c(all_variables, "alpha"),
+  left_join(tibble("variables" = c(all_variables, "beta"),
                    "National" = unname(
                      c(estimates[all_variables], estimates[
                        glue("lag(infectivesNational, {lag}):",
@@ -254,7 +234,7 @@ for (region in regions){
   
   # Insert parameter estimates in the results table
   results_table_aic = results_table_aic %>%
-    left_join(tibble("variables" = c(all_variables, "alpha"),
+    left_join(tibble("variables" = c(all_variables, "beta"),
                      !!glue("{region}") := unname(
                        c(estimates[all_variables], estimates[
                          glue("lag({infective_variable}, {lag}):",
@@ -270,7 +250,7 @@ for (region in regions){
 results_table_aic = results_table_aic %>%
   gather(region, val, 2:ncol(results_table_aic)) %>%
   spread(names(results_table_aic)[1], val) %>%
-  select(region, alpha, everything())
+  select(region, beta, everything())
 
 # Put the national results at the top
 results_table_aic = rbind(
@@ -283,7 +263,7 @@ table_ms_aic = xtable(results_table_aic, math.style.exponents = TRUE)
 table_ms_aic
 
 #### Models with model selection (BIC) ####
-results_table_bic = tibble(variables = c(all_variables, "alpha"))
+results_table_bic = tibble(variables = c(all_variables, "beta"))
 
 #### National model ####
 # Construct formula
@@ -293,7 +273,7 @@ fm = glue("infectivesNational ~ ",
   as.formula
 
 # Use BIC for model selection - scope says we want to always keep
-# alpha_within in
+# beta_within in
 model = step(lm(fm, data=df_wide), k=log(nrow(df_wide)), trace=0,
              scope=list("lower" = glue("infectivesNational ~ ",
                                        "lag(infectivesNational, {lag}):",
@@ -313,7 +293,7 @@ pvals = coef(summary(model))[, "Pr(>|t|)"] # TODO: SE with stars
 
 # Insert parameter estimates in the results table
 results_table_bic = results_table_bic %>%
-  left_join(tibble("variables" = c(all_variables, "alpha"),
+  left_join(tibble("variables" = c(all_variables, "beta"),
                    "National" = unname(
                      c(estimates[all_variables], estimates[
                        glue("lag(infectivesNational, {lag}):",
@@ -355,7 +335,7 @@ for (region in regions){
   
   # Insert parameter estimates in the results table
   results_table_bic = results_table_bic %>%
-    left_join(tibble("variables" = c(all_variables, "alpha"),
+    left_join(tibble("variables" = c(all_variables, "beta"),
                      !!glue("{region}") := unname(
                        c(estimates[all_variables], estimates[
                          glue("lag({infective_variable}, {lag}):",
@@ -371,7 +351,7 @@ for (region in regions){
 results_table_bic = results_table_bic %>%
   gather(region, val, 2:ncol(results_table_bic)) %>%
   spread(names(results_table_bic)[1], val) %>%
-  select(region, alpha, everything())
+  select(region, beta, everything())
 
 # Put the national results at the top
 results_table_bic = rbind(
@@ -383,7 +363,7 @@ results_table_bic = rbind(
 table_ms_bic = xtable(results_table_bic, math.style.exponents = TRUE)
 table_ms_bic
 
-#### Plot alpha over time ####
+#### Plot beta over time ####
 df_meta = readxl::read_xlsx(path_metadata, sheet = "Metadata")
 
 # Starting index - we want at least this number of observations
@@ -395,11 +375,11 @@ fm = glue("{infective_variable} ~ ",
   as.formula
 
 #### Without model selection ####
-tbl_alpha = tibble(date = as.Date(NA), alphas=numeric(0), code=character(0))
+tbl_beta = tibble(date = as.Date(NA), betas=numeric(0), code=character(0))
 
-# Find the estimates of alpha per region over time
+# Find the estimates of beta per region over time
 for (region in regions){
-  alphas = vector("double")
+  betas = vector("double")
   dates = vector("character")
   
   # Select only the data for the relevant region
@@ -409,45 +389,45 @@ for (region in regions){
     # Estimate the model by OLS
     model = lm(fm, data=data[1:t, ]) # (data[t-start+1:t,])
     
-    # Retrieve the alpha estimate and append this to the list of alphas
-    alpha = coef(model)[[glue("lag({infective_variable}, {lag}):",
+    # Retrieve the beta estimate and append this to the list of betas
+    beta = coef(model)[[glue("lag({infective_variable}, {lag}):",
                               "lag(susceptibleRate, {lag})")]]
-    alphas = c(alphas, alpha)
+    betas = c(betas, beta)
   }
   
   # Append the results to the table
-  tbl_alpha = tbl_alpha %>%
+  tbl_beta = tbl_beta %>%
     bind_rows(tibble(date = data$date[start:nrow(data)],
-                     alphas = alphas,
+                     betas = betas,
                      code = region) %>%
                 drop_na())
 }
 
 # Add region and direction to the table
-tbl_alpha = tbl_alpha %>%
+tbl_beta = tbl_beta %>%
   left_join(df_meta %>% select(c(region, code, direction)), by="code")
 
 # Make a plot per direction
-for (sub_tbl in split(tbl_alpha, tbl_alpha$direction)){
+for (sub_tbl in split(tbl_beta, tbl_beta$direction)){
   direc = sub_tbl$direction[1]
-  g = ggplot(sub_tbl, aes(date, alphas, colour = region)) + 
+  g = ggplot(sub_tbl, aes(date, betas, colour = region)) + 
     geom_point() +
     geom_smooth(method="loess", span=0.3, se=FALSE) +
     xlab("") +
-    ylab(TeX("$\\alpha_{within}$")) +
+    ylab(TeX("$\\beta_{within}$")) +
     scale_colour_manual(values=c("#E69F00", "#56B4E9", "#009E73", "#0072B2",
                                  "#D55E00", "#CC79A7"))
   print(g)
-  ggsave(glue("model1_lag{lag}_alphawithin_{direc}{undoc_flag}.pdf"),
+  ggsave(glue("model1_lag{lag}_betawithin_{direc}{undoc_flag}.pdf"),
          path=output_path, width = 10.8, height = 6.62, units = "in")
 }
 
 #### With model selection (AIC) ####
-tbl_alpha = tibble(date = as.Date(NA), alphas=numeric(0), code=character(0))
+tbl_beta = tibble(date = as.Date(NA), betas=numeric(0), code=character(0))
 
-# Find the estimates of alpha per region over time
+# Find the estimates of beta per region over time
 for (region in regions){
-  alphas = vector("double")
+  betas = vector("double")
   dates = vector("character")
   
   # Select only the data for the relevant region
@@ -455,7 +435,7 @@ for (region in regions){
   
   for (t in start:nrow(data)){
     # Use AIC for model selection - scope says we want to always keep
-    # alpha_within in
+    # beta_within in
     model = step(lm(fm, data=data[1:t, ]), k=2, trace=0,
                  scope=list("lower" = glue("{infective_variable} ~ ",
                                            "lag({infective_variable}, {lag}):",
@@ -463,45 +443,45 @@ for (region in regions){
                               as.formula,
                             "upper" = fm))
     
-    # Retrieve the alpha estimate and append this to the list of alphas
-    alpha = coef(model)[[glue("lag({infective_variable}, {lag}):",
+    # Retrieve the beta estimate and append this to the list of betas
+    beta = coef(model)[[glue("lag({infective_variable}, {lag}):",
                               "lag(susceptibleRate, {lag})")]]
-    alphas = c(alphas, alpha)
+    betas = c(betas, beta)
   }
   
   # Append the results to the table
-  tbl_alpha = tbl_alpha %>%
+  tbl_beta = tbl_beta %>%
     bind_rows(tibble(date = data$date[start:nrow(data)],
-                     alphas = alphas,
+                     betas = betas,
                      code = region) %>%
                 drop_na())
 }
 
 # Add region and direction to the table
-tbl_alpha = tbl_alpha %>%
+tbl_beta = tbl_beta %>%
   left_join(df_meta %>% select(c(region, code, direction)), by="code")
 
 # Make a plot per direction
-for (sub_tbl in split(tbl_alpha, tbl_alpha$direction)){
+for (sub_tbl in split(tbl_beta, tbl_beta$direction)){
   direc = sub_tbl$direction[1]
-  g = ggplot(sub_tbl, aes(date, alphas, colour = region)) + 
+  g = ggplot(sub_tbl, aes(date, betas, colour = region)) + 
     geom_point() +
     geom_smooth(method="loess", span=0.3, se=FALSE) +
     xlab("") +
-    ylab(TeX("$\\alpha_{within}$")) +
+    ylab(TeX("$\\beta_{within}$")) +
     scale_colour_manual(values=c("#E69F00", "#56B4E9", "#009E73", "#0072B2",
                                  "#D55E00", "#CC79A7"))
   print(g)
-  ggsave(glue("model1_lag{lag}_alphawithin_{direc}_aic{undoc_flag}.pdf"),
+  ggsave(glue("model1_lag{lag}_betawithin_{direc}_aic{undoc_flag}.pdf"),
          path=output_path, width = 10.8, height = 6.62, units = "in")
 }
 
 #### With model selection (BIC) ####
-tbl_alpha = tibble(date = as.Date(NA), alphas=numeric(0), code=character(0))
+tbl_beta = tibble(date = as.Date(NA), betas=numeric(0), code=character(0))
 
-# Find the estimates of alpha per region over time
+# Find the estimates of beta per region over time
 for (region in regions){
-  alphas = vector("double")
+  betas = vector("double")
   dates = vector("character")
   
   # Select only the data for the relevant region
@@ -509,7 +489,7 @@ for (region in regions){
   
   for (t in start:nrow(data)){
     # Use BIC for model selection - scope says we want to always keep
-    # alpha_within in
+    # beta_within in
     model = step(lm(fm, data=data[1:t, ]), k=log(t), trace=0,
                  scope=list("lower" = glue("{infective_variable} ~ ",
                                            "lag({infective_variable}, {lag}):",
@@ -517,35 +497,35 @@ for (region in regions){
                               as.formula,
                             "upper" = fm))
     
-    # Retrieve the alpha estimate and append this to the list of alphas
-    alpha = coef(model)[[glue("lag({infective_variable}, {lag}):",
+    # Retrieve the beta estimate and append this to the list of betas
+    beta = coef(model)[[glue("lag({infective_variable}, {lag}):",
                               "lag(susceptibleRate, {lag})")]]
-    alphas = c(alphas, alpha)
+    betas = c(betas, beta)
   }
   
   # Append the results to the table
-  tbl_alpha = tbl_alpha %>%
+  tbl_beta = tbl_beta %>%
     bind_rows(tibble(date = data$date[start:nrow(data)],
-                     alphas = alphas,
+                     betas = betas,
                      code = region) %>%
                 drop_na())
 }
 
 # Add region and direction to the table
-tbl_alpha = tbl_alpha %>%
+tbl_beta = tbl_beta %>%
   left_join(df_meta %>% select(c(region, code, direction)), by="code")
 
 # Make a plot per direction
-for (sub_tbl in split(tbl_alpha, tbl_alpha$direction)){
+for (sub_tbl in split(tbl_beta, tbl_beta$direction)){
   direc = sub_tbl$direction[1]
-  g = ggplot(sub_tbl, aes(date, alphas, colour = region)) + 
+  g = ggplot(sub_tbl, aes(date, betas, colour = region)) + 
     geom_point() +
     geom_smooth(method="loess", span=0.3, se=FALSE) +
     xlab("") +
-    ylab(TeX("$\\alpha_{within}$")) +
+    ylab(TeX("$\\beta_{within}$")) +
     scale_colour_manual(values=c("#E69F00", "#56B4E9", "#009E73", "#0072B2",
                                  "#D55E00", "#CC79A7"))
   print(g)
-  ggsave(glue("model1_lag{lag}_alphawithin_{direc}_bic{undoc_flag}.pdf"),
+  ggsave(glue("model1_lag{lag}_betawithin_{direc}_bic{undoc_flag}.pdf"),
          path=output_path, width = 10.8, height = 6.62, units = "in")
 }
