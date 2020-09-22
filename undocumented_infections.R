604568d02ee49383f1f2e9f790a4c6b910a9da17
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2020-09-22 22:00:45 +0200

    Update notation to Smax instead of N

--- undocumented_infections.R
+++ undocumented_infections.R
@@ -2,17 +2,17 @@ library(assertthat)
 library(glue)
 library(rootSolve)
 
-undocumented_infections = function(N, TC, form=c("Linear", "Quadratic",
+undocumented_infections = function(Smax, TC, form=c("Linear", "Quadratic",
                                                  "DownwardsVertex",
                                                  "UpwardsVertex", "Cubic"),
                                    beta=1/2, beta1=1/4, gamma=1/4, gamma2=1/2,
                                    fmin=0){
   
-  assert_that(!is.na(N) & !is.na(TC) & !is.na(fmin),
-              msg="N, TC, and fmin cannot be NA.")
+  assert_that(!is.na(Smax) & !is.na(TC) & !is.na(fmin),
+              msg="Smax, TC, and fmin cannot be NA.")
   
   if (form=="Linear") {
-    a = (1-fmin)/N
+    a = (1-fmin)/Smax
     b = fmin
     
     return(a*TC + b)
@@ -24,31 +24,31 @@ undocumented_infections = function(N, TC, form=c("Linear", "Quadratic",
     assert_that(!is.na(gamma),
                 msg=glue("For the functional form {form}, gamma cannot be NA."))
     
-    a = (beta - gamma + (1-beta)*fmin)/(beta*(1-beta)*N^2)
-    b = (gamma - beta^2 - (1-beta^2)*fmin)/(beta*(1-beta)*N)
+    a = (beta - gamma + (1-beta)*fmin)/(beta*(1-beta)*Smax^2)
+    b = (gamma - beta^2 - (1-beta^2)*fmin)/(beta*(1-beta)*Smax)
     c = fmin
     
-    # To ensure that the vertex lies beyond the domain (0, N), we check that if
-    # there is an extremum in the interval [0, N], that it has to be at 0 or N.
-    roots = uniroot.all(function(x){2*a*x + b}, c(0,N))
+    # To ensure that the vertex lies beyond the domain (0, Smax), we check that if
+    # there is an extremum in the interval [0, Smax], that it has to be at 0 or Smax.
+    roots = uniroot.all(function(x){2*a*x + b}, c(0,Smax))
     if (length(roots) == 1) {
-      assert_that((roots == 0) | (roots == N),
+      assert_that((roots == 0) | (roots == Smax),
                   msg = glue("For the functional form {form}, the vertex ",
-                             "should have TC_t<=0 or TC_t>={N}. However, it ",
+                             "should have TC_t<=0 or TC_t>={Smax}. However, it ",
                              "is equal to {roots}."))
     }
     
     return(a*TC^2 + b*TC + c)
     
   } else if (form == "DownwardsVertex") {
-    a = (fmin-1)/N^2
-    b = 2*(fmin-1)/N
+    a = (fmin-1)/Smax^2
+    b = 2*(fmin-1)/Smax
     c = fmin
     
     return(a*TC^2 + b*TC + c)
     
   } else if (form == "UpwardsVertex") {
-    a = (1-fmin)/N^2
+    a = (1-fmin)/Smax^2
     b = 0
     c = fmin
     
@@ -71,12 +71,12 @@ undocumented_infections = function(N, TC, form=c("Linear", "Quadratic",
                 msg=glue("For the functional form {form}, beta1 should be ",
                          "less than beta."))
     
-    a = (8 + 64*gamma - 48*gamma2 - 24*fmin)/(3*N^3)
-    b = (-2 - 32*gamma + 20*gamma2 + 14*fmin)/(N^2)
-    c = (1 + 32*gamma - 12*gamma2 - 21*fmin)/(3*N)
+    a = (8 + 64*gamma - 48*gamma2 - 24*fmin)/(3*Smax^3)
+    b = (-2 - 32*gamma + 20*gamma2 + 14*fmin)/(Smax^2)
+    c = (1 + 32*gamma - 12*gamma2 - 21*fmin)/(3*Smax)
     d = fmin
     
-    roots = uniroot.all(function(x){ 3*a*x^2 + 2*b*x + c }, c(0, N))
+    roots = uniroot.all(function(x){ 3*a*x^2 + 2*b*x + c }, c(0, Smax))
     assert_that(length(roots) %in% c(0,1),
                 msg=glue("For the functional form {form}, at most 1 extremum ",
                          "should occur but there are {length(roots)}."))
